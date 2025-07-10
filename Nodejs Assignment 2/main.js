0da5d452e59391f9bebfe98fa740a3eacfdc73c0
Darur Vinay <105033404+vinaydarur123@users.noreply.github.com>
2025-07-10 09:47:36 +0530

    Update main.js

--- Nodejs Assignment 2/main.js
+++ Nodejs Assignment 2/main.js
@@ -1,1 +1,16 @@
+import readline from 'readline';
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('Enter a word: ', async (word) => {
+    if (word) {
+        const { capitalize } = await import('./stringUtils.js');
+        console.log(`Capitalized: ${capitalize(word)}`);
+    } else {
+        console.log('No word entered.');
+    }
+    rl.close();
+});
