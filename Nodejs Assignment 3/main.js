7e09f17f9e4b76d7a42e3f8f31eaa228ec0c14a4
Darur Vinay <105033404+vinaydarur123@users.noreply.github.com>
2025-07-10 09:50:21 +0530

    Update main.js

--- Nodejs Assignment 3/main.js
+++ Nodejs Assignment 3/main.js
@@ -1,7 +1,7 @@
 import User, { validateEmail } from './user.js';
 
-const user = new User('Jayatheertha');
+const user = new User('Darur Vinay');
 console.log(user.getInfo());
 
-const email = 'jayatheertha2381@gmail.com';
+const email = 'vinaydarur123@gmail.com';
 console.log(`Is "${email}" a valid email?`, validateEmail(email));
