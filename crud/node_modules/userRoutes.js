0afb13551e4470b55a1db5d70557d899b4001c9a
Darur Vinay <105033404+vinaydarur123@users.noreply.github.com>
2025-07-24 09:50:30 +0530

    Add files via upload

---
+++ crud/node_modules/userRoutes.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+
+// CREATE
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+// READ ALL
+router.get('/', async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
+
+
+// READ ONE
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).json({ error: 'User not found' });
+        res.json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedUser) return res.status(404).json({ error: 'User not found' });
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) return res.status(404).json({ error: 'User not found' });
+        res.json({ message: 'User deleted' });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+
+module.exports = router;
